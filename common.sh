f0ab9ea5b0a1bc268e759d3aca710bb1cdcd4947
Michael Roth <michael.roth@amd.com>
2025-10-22 17:37:11 -0500

    common.sh: Fix handling for default guest config path
    A bug in the current scripts will always try to use custom-specified
    guest config, even if a custom config hasn't been specified. Fix this
    logic to only set it if a custom guest config is specific, just like
    the logic for custom host configs does.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- common.sh
+++ common.sh
@@ -41,12 +41,13 @@ build_kernel()
 
 		if [ "${V}" = "guest" ]; then
 			BRANCH="${KERNEL_GUEST_BRANCH}"
-			kernel_config_path=${KERNEL_GUEST_CONFIG_TEMPLATE}
+			KERNEL_GIT_URL="${KERNEL_GUEST_GIT_URL:-${KERNEL_GIT_URL}}"
 			if [ -n "$KERNEL_GUEST_CONFIG_TEMPLATE" ]; then
 				kernel_config_path=${KERNEL_GUEST_CONFIG_TEMPLATE}
 			fi
 		else
 			BRANCH="${KERNEL_HOST_BRANCH}"
+			KERNEL_GIT_URL="${KERNEL_GUEST_GIT_URL:-${KERNEL_GIT_URL}}"
 			if [ -n "$KERNEL_HOST_CONFIG_TEMPLATE" ]; then
 				kernel_config_path=${KERNEL_HOST_CONFIG_TEMPLATE}
 			fi
